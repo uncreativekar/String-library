c49139d6335d934da5fce2fbd9b87fcb522d6c96
uncreativekar <andyaalgaard@gmail.com>
2020-03-08 02:51:38 +0100

    Added widestring support

--- String library/String.cpp
+++ String library/String.cpp
@@ -243,6 +243,8 @@ String String::getline(std::istream& is, const char& end) const
 			break;
 
 	}
+	
+	free(buffer);
 	return retvalue;
 }
 
@@ -367,6 +369,397 @@ String::operator std::string() const
 }
 
 std::ostream& operator<<(std::ostream& outstream, String const& obj)
+{
+	outstream << obj.m_buffer;
+	return outstream;
+}
+
+
+/* WIDE SHIT*/
+wString::wString()
+{
+
+}
+
+wString::wString(const wString& obj)
+{
+	int len = obj.size() * sizeof(wchar_t);
+
+	m_buffer = (wchar_t*)malloc(len + 2);
+	if (m_buffer == 0)
+		throw std::runtime_error("Nullptr exception in wString::wString(const wString&)");
+	memset(m_buffer, 0, len + 2);
+	memcpy(m_buffer, obj.m_buffer, len);
+}
+
+wString& wString::operator=(const wString& obj)
+{
+	int len = obj.size() * 2;
+
+	m_buffer = (wchar_t*)malloc(len + 2);
+	if (m_buffer == 0)
+		throw std::runtime_error("Nullptr exception in wString::wString(const wString&)");
+	memset(m_buffer, 0, len + 2);
+	memcpy(m_buffer, obj.m_buffer, len);
+
+	return *this;
+}
+
+wString::wString(wchar_t const* str)
+{
+	if (str == 0)
+		return;
+
+	int iSize = wcslen(str) * sizeof(wchar_t);
+
+	if (iSize < 1)
+		return;
+
+	m_buffer = (wchar_t*)malloc(iSize + 2);
+	if (m_buffer == 0)
+		throw std::runtime_error("Nullptr exception in wString::wString(wchar_t const*)");
+	memset(m_buffer, 0, iSize + 2);
+	memcpy(m_buffer, str, iSize);
+	
+
+}
+
+wString::~wString()
+{
+	free(m_buffer);
+}
+
+size_t wString::size() const
+{
+	if (m_buffer == 0)
+		return 0;
+
+	return wcslen(m_buffer);
+}
+
+bool wString::empty() const
+{
+	return this->size() < 1;
+}
+
+wString wString::substr(int start, int length) const
+{
+	if (length == -1)
+		length = (this->size()) - start;
+
+	int len = length * 2;
+	/* Create temporary buffer */
+	wchar_t* buffer = (wchar_t*)malloc(len + 2);
+	if (buffer == 0)
+		throw std::runtime_error("Nullptr exception in wString::substr(int,int)");
+	memset(buffer, 0, length + 2);
+	memcpy(buffer, m_buffer + start, length * 2);
+
+	wString obj(buffer);
+	free(buffer);
+
+	return obj;
+}
+
+bool wString::compare(wString const& obj) const
+{
+	if (this->size() != obj.size())
+		return false;
+
+	for (size_t index = 0; index < this->size(); ++index)
+	{
+		if (this->m_buffer[index] != obj.m_buffer[index])
+			return false;
+	}
+
+	return true;
+}
+
+wString wString::lower() const
+{
+	int len = this->size() * 2;
+
+	wchar_t* buffer = (wchar_t*)malloc(len + 2); /* Create temporary buffer */
+	if (buffer == 0)
+		throw std::runtime_error("Nullptr exception in wString::lower()");
+	memset(buffer, 0, len + 2);
+
+	for (size_t index = 0; index < this->size(); ++index)
+	{
+		buffer[index] = towlower(m_buffer[index]);
+	}
+	buffer[this->size()] = 0;
+
+	wString str(buffer);
+	free(buffer);
+	return str;
+}
+
+wString wString::upper() const
+{
+	int len = this->size() * 2;
+
+	wchar_t* buffer = (wchar_t*)malloc(len + 2); /* Create temporary buffer */
+	if (buffer == 0)
+		throw std::runtime_error("Nullptr exception in wString::lower()");
+	memset(buffer, 0, len + 2);
+
+	for (uint64_t index = 0; index < this->size(); ++index)
+	{
+		buffer[index] = towupper(m_buffer[index]);
+	}
+
+	wString str(buffer);
+	free(buffer);
+	return str;
+}
+
+void wString::replace_one(wString const& to_replace, wString const& NewwString)
+{
+	if (this->find(to_replace) == -1)
+		return;
+
+	int new_size = ((this->size() - to_replace.size()) + NewwString.size()) * 2;
+
+
+	wchar_t* buffer = (wchar_t*)malloc(new_size + 2);
+	if (buffer == 0)
+		throw std::runtime_error("Nullptr exception in wString::replace_one(wString const&, wString const&)");
+	memset(buffer, 0, new_size + 2);
+
+	memcpy(buffer, m_buffer, this->find(to_replace) * 2);
+
+	if (!NewwString.empty())
+		wcscat(buffer + this->find(to_replace), NewwString.c_str());
+	wcscat(buffer + this->find(to_replace) + NewwString.size(), m_buffer + this->find(to_replace) + to_replace.size());
+
+	free(m_buffer);
+	m_buffer = buffer;
+}
+
+void wString::replace(wString const& to_replace, wString const& NewwString)
+{
+
+	while (this->find(to_replace) != -1)
+	{
+		this->replace_one(to_replace, NewwString);
+	}
+}
+
+size_t wString::find(wString const& to_find, int start_pos) const
+{
+	if (this->size() < to_find.size())
+		return -1;
+
+
+	for (size_t index = start_pos; index < this->size(); ++index)
+	{
+		if (wmemcmp(m_buffer + index, to_find.c_str(), to_find.size()) == 0)
+		{
+			return index;
+		}
+	}
+
+	return -1;
+}
+
+size_t wString::find_last(wString const& to_find, int start_pos) const
+{
+	size_t pos = start_pos;
+	size_t lpos = start_pos;
+	while (this->find(to_find, pos) != -1) {
+		lpos = this->find(to_find, pos);
+		++pos;
+	}
+
+	return lpos;
+}
+
+wchar_t const* wString::c_str() const
+{
+	return (wchar_t const*)m_buffer;
+}
+
+std::vector<wString> wString::split(wString const& delimiter) const
+{
+	std::vector<wString> wStrings;
+
+	size_t prev = 0, pos = 0;
+	do
+	{
+		pos = this->find(delimiter, prev);
+		if (pos == -1) pos = this->size();
+		wString token = this->substr(prev, pos - prev);
+		if (!token.empty())
+			wStrings.push_back(token);
+		prev = pos + delimiter.size();
+	} while (pos < this->size() && prev < this->size());
+
+	return wStrings;
+}
+
+wString wString::getline(std::wistream& is, const wchar_t& end) const
+{
+	int bufsize = 1024 * 2;
+
+	wchar_t* buffer = (wchar_t*)malloc(bufsize + 2);
+	if (buffer == 0)
+		throw std::runtime_error("Nullptr exception in wString::getline(std::istream&, const wchar_t&)");
+	memset(buffer, 0, bufsize + 2);
+
+	wString retvalue;
+
+	while (true)
+	{
+		is.get(buffer, bufsize);
+
+		for (unsigned int index = 0; index < wcslen(buffer); ++index)
+		{
+			if (buffer[index] == end) {
+
+				buffer[index + 1] = 0;
+			}
+		}
+
+		if (wcslen(buffer) > 1) {
+			retvalue.append(buffer);
+		}
+		else
+			break;
+
+	}
+
+	free(buffer);
+	return retvalue;
+}
+
+bool wString::is_number() const
+{
+	for (unsigned int index = 0; index < this->size(); ++index)
+	{
+		if (!std::isdigit(m_buffer[index], std::locale()))
+			return false;
+	}
+
+	return true;
+}
+
+void wString::append(wString const& obj)
+{
+	int len = ((this->size() + obj.size()) * sizeof(wchar_t)) + 2;
+
+	wchar_t* buffer = (wchar_t*)malloc(this->size() + obj.size() + 2);
+	if (buffer == 0)
+		throw std::runtime_error("Nullptr exception in wString::append(wString const&)");
+	memset(buffer, 0, len);
+
+	memcpy(buffer, m_buffer, this->size() * sizeof(wchar_t));
+	wcscat(buffer + this->size(), obj.m_buffer);
+
+	
+
+	free(buffer);
+	m_buffer = buffer;
+}
+
+void wString::append(wchar_t c)
+{
+	wchar_t buf[2] = { c, 0 };
+	this->append((wchar_t const*)&buf);
+}
+
+void wString::pop_back()
+{
+	int len = this->size() * 2;
+
+	wchar_t* buffer = (wchar_t*)malloc(len);
+	if (buffer == 0)
+		throw std::runtime_error("Nullptr exception in wString::pop_back");
+	memset(buffer, 0, len);
+	memcpy(buffer, m_buffer, len - 2);
+
+	free(buffer);
+	m_buffer = buffer;
+}
+
+bool wString::operator==(wString const& obj) const
+{
+	return this->compare(obj);
+}
+
+bool wString::operator!=(wString const& obj) const
+{
+	return !this->compare(obj);
+}
+
+bool wString::operator<(wString const& obj) const
+{
+	return this->size() < obj.size();
+}
+
+bool wString::operator<=(wString const& obj) const
+{
+	return this->size() <= obj.size();
+}
+
+bool wString::operator>(wString const& obj) const
+{
+	return this->size() > obj.size();
+}
+
+bool wString::operator>=(wString const& obj) const
+{
+	return this->size() >= obj.size();
+}
+
+wString wString::operator*(size_t times) const
+{
+	wString str;
+
+	for (size_t index = 0; index < times; ++index)
+	{
+		str.append(*this);
+	}
+
+	return str;
+}
+
+void wString::operator*=(size_t times)
+{
+	if (times > 1)
+		--times;
+
+	wString original(m_buffer);
+	for (int index = 0; index < times; ++index)
+	{
+		this->append(original);
+	}
+}
+
+wString wString::operator+(wString const& obj) const
+{
+	wString NewwString(m_buffer);
+	NewwString.append(obj);
+
+	return NewwString;
+}
+
+void wString::operator+=(wString const& obj)
+{
+	this->append(obj);
+}
+
+bool wString::operator!() const
+{
+	return this->empty();
+}
+
+wString::operator std::wstring() const
+{
+	return std::wstring(m_buffer);
+}
+
+std::wostream& operator<<(std::wostream& outstream, wString const& obj)
 {
 	outstream << obj.m_buffer;
 	return outstream;
--- String library/String.h
+++ String library/String.h
@@ -152,3 +152,150 @@ private:
 
 };
 
+
+class wString
+{
+public:
+	/* Constructor */
+	wString();
+
+	/* Copy constructor */
+	wString(const wString& obj);
+
+	/* Assignment operator */
+	wString& operator=(const wString& obj);
+
+	/* Constructor */
+	wString(wchar_t const* str);
+
+	/* Destructor */
+	~wString();
+
+	/* returns the size of the string, ends on null terminator */
+	size_t size() const;
+
+	/* returns true if the string is empty */
+	bool empty() const;
+
+	/* Substring function, returns part of string */
+	wString substr(int start, int length = -1) const;
+
+	/* Compare function, returns true if the strings are equal */
+	bool compare(wString const& obj) const;
+
+	/* returns lowercase string */
+	wString lower() const;
+
+	/* returns uppercase string */
+	wString upper() const;
+
+	/* replaces one occurrence of string */
+	void replace_one(wString const& to_replace, wString const& NewString);
+
+	/* replaces all occurrences of string */
+	void replace(wString const& to_replace, wString const& NewString);
+
+	/* returns the position of first occurence in string*/
+	size_t find(wString const& to_find, int start_pos = 0) const;
+
+	/* returns the position of last found occurence in string*/
+	size_t find_last(wString const& to_find, int start_pos = 0) const;
+
+	/* Returns the underlying string buffer */
+	wchar_t const* c_str() const;
+
+	/* Split function, returns vector of strings */
+	std::vector<wString> split(wString const& delimiter) const;
+
+	/* get function */
+	wString getline(std::wistream& is, const wchar_t& end = '\n') const;
+
+	/* to_number function template */
+	template <typename T>
+	T to_number(unsigned int base = 10) const
+	{
+		return std::stoi(std::wstring(m_buffer), 0, base);
+	}
+
+	/* to_number double function template */
+	template <>
+	double to_number(unsigned int base) const
+	{
+		return std::stod(std::wstring(m_buffer));
+	}
+
+	/* to_number unsigned int function */
+	template <>
+	unsigned long to_number(unsigned int base) const
+	{
+		return std::stoul(std::wstring(m_buffer), 0, base);
+	}
+
+	/* to_number long long short function */
+	template <>
+	long long to_number(unsigned int base) const
+	{
+		return std::stoll(std::wstring(m_buffer), 0, base);
+	}
+
+	/* to_number function template */
+	template <>
+	unsigned long long to_number(unsigned int base) const
+	{
+		return std::stoull(std::wstring(m_buffer), 0, base);
+	}
+
+	/* returns true if all characters are numbers */
+	bool is_number() const;
+
+	/* Append function */
+	void append(wString const& obj);
+
+	/* Append function overloaded */
+	void append(wchar_t c);
+
+	/* Pops back character */
+	void pop_back();
+
+	/* LShift operator overload */
+	friend std::wostream& operator<<(std::wostream& outstream, wString const& obj);
+
+	/* returns true if strings are equal */
+	bool operator==(wString const& obj) const;
+
+	/* returns true if string are not equal  */
+	bool operator!=(wString const& obj) const;
+
+	/* returns true if other string is less */
+	bool operator<(wString const& obj) const;
+
+	/* returns true if other string is less or equal size */
+	bool operator<=(wString const& obj) const;
+
+	/* retuns true if string is larger size than other string */
+	bool operator>(wString const& obj) const;
+
+	/* returns true if string is larger or equal size to other string */
+	bool operator>=(wString const& obj) const;
+
+	/* multiplies string by times */
+	wString operator*(size_t times) const;
+
+	/* multiplies string by times */
+	void operator*=(size_t times);
+
+	/* appends other string */
+	wString operator+(wString const& obj) const;
+
+	/* appends other string */
+	void operator+=(wString const& obj);
+
+	/* returns true if string is empty */
+	bool operator!() const;
+
+	/* std::string cast */
+	operator std::wstring() const;
+private:
+	wchar_t* m_buffer = 0;
+
+};
